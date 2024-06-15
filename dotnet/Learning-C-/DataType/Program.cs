f0d71db9b09f15d6b544ae15d7ebae5b06b3e056
Rayhona Jalilova <153175304+rayhona-jalilova@users.noreply.github.com>
2024-06-15 10:15:54 +0300

    Add files via upload

---
+++ dotnet/Learning-C-/DataType/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Hello, World!");
+
+string firstFriend = "Maria";
+
+firstFriend = firstFriend.Trim();
+
+string secondFriend = "Scott";
+
+string friends = $"My friends are {firstFriend} and {secondFriend.Trim()}";
+
+
+Console.WriteLine(friends.ToUpper());
+
+Console.WriteLine(friends);
